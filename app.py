f2aaad9ccaef33434e98153de743b6b6b4f9843b
Nereo Manganiello <nereomanganiello@gmail.com>
2025-03-29 20:35:37 -0300

    cambios

--- app.py
+++ app.py
@@ -451,6 +451,7 @@ def cambiar_modo():
 if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
 
   
